ff067f992110561d83b23cd75f2e412aeebc705d
kingdaro <kingdaro@gmail.com>
2020-02-24 11:38:32 -0600

    split up code so just the github stuff can be mocked

--- src/github.ts
+++ src/github.ts
@@ -3,7 +3,7 @@ import { context, GitHub } from "@actions/github"
 import { Octokit } from "@octokit/rest"
 
 export type PullRequest = Octokit.PullsListResponseItem
-type CheckRun = Octokit.ChecksListForRefResponseCheckRunsItem
+export type CheckRun = Octokit.ChecksListForRefResponseCheckRunsItem
 
 const token = core.getInput("token")
 const github = new GitHub(token)
@@ -13,26 +13,13 @@ const repoParams = {
   repo: context.repo.repo,
 }
 
-export const hasLabel = (pr: PullRequest, name: string) =>
-  pr.labels.some((label) => label.name.toLowerCase() === name.toLowerCase())
-
-export const doesNotHaveLabel = (pr: PullRequest, name: string) =>
-  pr.labels.every((label) => label.name.toLowerCase() !== name.toLowerCase())
-
-export const hasPassingChecks = async (pr: PullRequest) => {
-  const { data: checks } = await github.checks.listForRef({
+export const getChecks = (pr: PullRequest) =>
+  github.checks.listForRef({
     ...repoParams,
     ref: pr.head.ref,
   })
-  return checks.check_runs.every(isCheckRunPassing)
-}
-
-const isCheckRunPassing = (run: CheckRun) =>
-  run.status === "completed" && run.conclusion === "success"
 
 export const getPullRequests = () => github.pulls.list(repoParams)
 
 export const mergePullRequest = (pr: PullRequest) =>
   github.pulls.merge({ ...repoParams, pull_number: pr.number })
-
-export const prHumanFormat = (pr: PullRequest) => `#${pr.number} (${pr.title})`
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,34 @@
 import * as core from "@actions/core"
 import {
-  doesNotHaveLabel,
+  CheckRun,
+  getChecks,
   getPullRequests,
-  hasLabel,
-  hasPassingChecks,
   mergePullRequest,
-  prHumanFormat,
   PullRequest,
 } from "./github"
 import { getErrorMessage } from "./helpers"
+
+const hasLabel = (pr: PullRequest, name: string) =>
+  pr.labels.some((label) => label.name.toLowerCase() === name.toLowerCase())
+
+const doesNotHaveLabel = (pr: PullRequest, name: string) =>
+  pr.labels.every((label) => label.name.toLowerCase() !== name.toLowerCase())
 
 const hasCorrectLabels = (pr: PullRequest) =>
   hasLabel(pr, "code review pass") &&
   hasLabel(pr, "qa pass") &&
   doesNotHaveLabel(pr, "wip") &&
   doesNotHaveLabel(pr, "do not merge")
+
+const hasPassingChecks = async (pr: PullRequest) => {
+  const { data: checks } = await getChecks(pr)
+  return checks.check_runs.every(isCheckRunPassing)
+}
+
+const isCheckRunPassing = (run: CheckRun) =>
+  run.status === "completed" && run.conclusion === "success"
+
+export const prHumanFormat = (pr: PullRequest) => `#${pr.number} (${pr.title})`
 
 async function run() {
   const result = await getPullRequests()
