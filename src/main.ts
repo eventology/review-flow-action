f41fb7c34d004e8d35f30e4a5379918a8cf88523
kingdaro <kingdaro@gmail.com>
2020-02-24 11:09:08 -0600

    split out helper functions

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,28 @@
 import * as core from "@actions/core"
-import { context, GitHub } from "@actions/github"
-import { Octokit } from "@octokit/rest"
+import {
+  doesNotHaveLabel,
+  getPullRequests,
+  hasLabel,
+  hasPassingChecks,
+  mergePullRequest,
+  prHumanFormat,
+  PullRequest,
+} from "./github"
+import { getErrorMessage } from "./helpers"
 
-type PullRequest = Octokit.PullsListResponseItem
-type CheckRun = Octokit.ChecksListForRefResponseCheckRunsItem
-
-const token = core.getInput("token")
-const github = new GitHub(token)
-
-const repoParams = {
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-}
-
-const shouldMerge = async (pr: PullRequest) =>
+const hasCorrectLabels = (pr: PullRequest) =>
   hasLabel(pr, "code review pass") &&
   hasLabel(pr, "qa pass") &&
   doesNotHaveLabel(pr, "wip") &&
-  doesNotHaveLabel(pr, "do not merge") &&
-  hasPassingChecks(pr)
-
-const hasLabel = (pr: PullRequest, name: string) =>
-  pr.labels.some((label) => label.name.toLowerCase() === name.toLowerCase())
-
-const doesNotHaveLabel = (pr: PullRequest, name: string) =>
-  pr.labels.every((label) => label.name.toLowerCase() !== name.toLowerCase())
-
-const hasPassingChecks = async (pr: PullRequest) => {
-  const { data: checks } = await github.checks.listForRef({
-    ...repoParams,
-    ref: pr.head.ref,
-  })
-  return checks.check_runs.every(isCheckRunPassing)
-}
-
-const isCheckRunPassing = (run: CheckRun) =>
-  run.status === "completed" && run.conclusion === "success"
-
-const prHumanFormat = (pr: PullRequest) => `#${pr.number} (${pr.title})`
-
-const getErrorMessage = (error: unknown) =>
-  error instanceof Error ? error.message : String(error)
-
-const merge = (pr: PullRequest) =>
-  github.pulls.merge({ ...repoParams, pull_number: pr.number })
+  doesNotHaveLabel(pr, "do not merge")
 
 async function run() {
-  const result = await github.pulls.list(repoParams)
+  const result = await getPullRequests()
 
   const tasks = result.data.map(async (pr) => {
     try {
-      if (await shouldMerge(pr)) {
-        await merge(pr)
+      if (hasCorrectLabels(pr) && (await hasPassingChecks(pr))) {
+        await mergePullRequest(pr)
         core.info(`Merged #${pr.number} (${pr.title})`)
       }
     } catch (error) {
