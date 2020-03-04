f75494f8552b674c97de0aeb243b910ed60127d0
kingdaro <kingdaro@gmail.com>
2020-03-04 13:48:06 -0600

    release script

---
+++ scripts/release.js
@@ -0,0 +1,40 @@
+// @ts-check
+/* eslint-disable no-console */
+const { exec, execSync } = require("child_process")
+const inquirer = require("inquirer")
+// @ts-ignore
+const pkg = require("../package.json")
+const { promises: fs } = require("fs")
+
+async function main() {
+  console.info(`Current version: ${pkg.version}`)
+
+  const { newVersion } = await inquirer.prompt([
+    { type: "input", name: "newVersion", message: "New version?" },
+  ])
+
+  // update package.json
+  await fs.writeFile(
+    `${__dirname}/../package.json`,
+    JSON.stringify({ ...pkg, version: newVersion }),
+  )
+
+  // build
+  execSync("yarn build")
+
+  // commit
+  execSync(`git commit -a -m "v${newVersion}"`)
+
+  // push to master
+  execSync(`git push origin master`)
+
+  // push to releases/{version}
+  execSync(`git push origin releases/${newVersion}`)
+
+  console.info("Success!")
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
